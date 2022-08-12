6b96db80d414a80e868e2523034b1e902f252ab4
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2022-08-12 14:57:56 +0200

    Styling

--- app/src/components/App/App.js
+++ app/src/components/App/App.js
@@ -1,13 +1,19 @@
 import MainRouting from './MainRouting';
 import Navigation from './Nav/Navigation';
+import "../../index.css"
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
-      <main>
+      <main class="flex-shrink-0">
         <MainRouting />
       </main>
+      <footer>
+
+      </footer>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" defer></script>
+      <script src="https://cdn.startbootstrap.com/sb-forms-latest.js" defer></script>
     </div>
   );
 }
