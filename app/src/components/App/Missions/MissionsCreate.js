253684a6baea44c94bbd88da820d85b1072f678e
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2022-08-24 12:59:14 +0200

    changed icon

--- app/src/components/App/Missions/MissionsCreate.js
+++ app/src/components/App/Missions/MissionsCreate.js
@@ -76,7 +76,7 @@ function MissionCreate() {
                 <Container>
                     <div className="bg-light rounded-3 py-5 px-4 px-md-5 mb-5">
                         <div className="text-center mb-5">
-                            <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i className="bi bi-lock"></i></div>
+                            <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i className="bi bi-file-earmark-plus"></i></div>
                             <h1 className="fw-bolder">Create Assignment</h1>
                         </div>
                         <div className='row gx-5 justify-content-center'>
